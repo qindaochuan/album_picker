648dea5251dbff6123eb0dc6e44ff9ee9d236917
jun zhong <>
2020-01-16 15:48:54 +0800

    no message

--- ios/Classes/AlbumPickerPlugin.m
+++ ios/Classes/AlbumPickerPlugin.m
@@ -1,20 +1,53 @@
 #import "AlbumPickerPlugin.h"
 
+@interface AlbumPickerPlugin()
+
+@property(copy, nonatomic) FlutterResult result;
+
+@end
+
 @implementation AlbumPickerPlugin
+{
+    NSDictionary *_arguments;
+    UIViewController *_viewController;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"album_picker"
+      methodChannelWithName:@"plugins.flutter.io/album_picker"
             binaryMessenger:[registrar messenger]];
-  AlbumPickerPlugin* instance = [[AlbumPickerPlugin alloc] init];
+    UIViewController* viewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+  AlbumPickerPlugin* instance = [[AlbumPickerPlugin alloc] initWithViewControllver:viewController];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
+- (instancetype)initWithViewControllver:(UIViewController *)viewController {
+    if(self){
+        _viewController = viewController;
+    }
+    return self;
+}
+
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    if (self.result) {
+        self.result([FlutterError errorWithCode:@"multiple_request"
+                                        message:@"Cancelled by a second request"
+                                        details:nil]);
+        self.result = nil;
+    }
+    
+  if ([@"pickFile" isEqualToString:call.method]) {
+      self.result = result;
+      _arguments = call.arguments;
+      [self pickFile];
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (void)pickFile{
+    NSLog(@"iOS called pickFile");
+    self.result(@"");
 }
 
 @end
