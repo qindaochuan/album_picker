fdc94b5e053f4a93d0a3a621357ed630dfd9a751
jun zhong <>
2020-04-09 10:58:28 +0800

    删除matisse

--- android/src/main/java/com/qianren2/album_picker/AlbumPickerDelegate.java
+++ android/src/main/java/com/qianren2/album_picker/AlbumPickerDelegate.java
@@ -6,12 +6,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 
-import com.zhihu.matisse.Matisse;
-import com.zhihu.matisse.MimeType;
-import com.zhihu.matisse.engine.impl.GlideEngine;
-import com.zhihu.matisse.engine.impl.PicassoEngine;
-import com.zhihu.matisse.filter.Filter;
-
 import java.lang.ref.WeakReference;
 
 import io.flutter.plugin.common.MethodCall;
@@ -72,16 +66,6 @@ public class AlbumPickerDelegate implements PluginRegistry.ActivityResultListene
                 super.run();
                 Looper.prepare();
                 System.out.println("AlbumPicker startActivity: MainActivity");
-                Matisse.from(activity)
-                        .choose(MimeType.ofImage())
-                        .theme(R.style.Matisse_Dracula)
-                        .countable(false)
-                        .addFilter(new GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
-                        .maxSelectable(9)
-                        .originalEnable(true)
-                        .maxOriginalSize(10)
-                        .imageEngine(new GlideEngine())
-                        .forResult();
                 Looper.loop();
             }
         }.start();
